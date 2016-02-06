9af280c6bcb0acc85963ff2624b6bab7978da0e6
Timothy Stewart <timothystewart6@hotmail.com>
2016-02-05 20:48:14 -0600

    removed application insights

--- LED_Click_8x8/App.xaml.cs
+++ LED_Click_8x8/App.xaml.cs
@@ -28,9 +28,6 @@ namespace LED_Click_8x8
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
